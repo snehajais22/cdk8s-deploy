ac59b1b7a84079ee3e220d84a48dffb8e4708a45
Sneha Jaiswal <sneha.jaiswal22@ibm.com>
2024-07-09 14:28:15 -0400

    trigger webhook

--- main.ts
+++ main.ts
@@ -30,7 +30,7 @@ class PipelineRunTest extends Chart {
       .withStep(new TaskStepBuilder()
         .withName('step')
         .withImage('ubuntu')
-        .fromScriptData('#!/usr/bin/env bash\necho $(params.url)\necho Almost there...'));
+        .fromScriptData('#!/usr/bin/env bash\necho $(params.url)\necho Anotha one'));
 
     const pvcProps : PersistentVolumeClaimProps = { metadata: { name: 'datapvc' }, accessModes: [PersistentVolumeAccessMode.READ_WRITE_ONCE], storage: Size.gibibytes(1) };
     new PersistentVolumeClaim(this, 'datapvc', pvcProps);
