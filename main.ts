2a958c11546eb9ea53e7de64844d408de848dce7
Sneha Jaiswal <sneha.jaiswal22@ibm.com>
2024-07-16 10:48:10 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -37,7 +37,7 @@ class PipelineRunTest extends Chart {
       .withStep(new TaskStepBuilder()
         .withName('echo')
         .withImage('ubuntu')
-        .fromScriptData('#!/usr/bin/env bash\necho This task runs after'));
+        .fromScriptData('#!/usr/bin/env bash\necho Check if these logs print after'));
 
     const pvcProps : PersistentVolumeClaimProps = { metadata: { name: 'datapvc' }, accessModes: [PersistentVolumeAccessMode.READ_WRITE_ONCE], storage: Size.gibibytes(1) };
     new PersistentVolumeClaim(this, 'datapvc', pvcProps);
